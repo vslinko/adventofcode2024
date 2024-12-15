6691274c260d0383e74a2539dc783814699963eb
Viacheslav Slinko <me@vslinko.ru>
2024-12-15 21:08:16 +0300

    codspeed

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,80 +1,37 @@
-use std::fs::read_to_string;
-
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use solution::day10;
-use solution::day11;
-use solution::day12;
-use solution::day13;
-use solution::day14;
-use solution::day9;
+use paste::paste;
 
-pub fn day9(c: &mut Criterion) {
-    let s = read_to_string("./inputs/9.txt").unwrap();
-    let s = s.as_str();
-
-    c.bench_function("bench_day9_part1", |b| b.iter(|| day9::part1(black_box(s))));
-    c.bench_function("bench_day9_part2", |b| b.iter(|| day9::part2(black_box(s))));
+macro_rules! get_day_input {
+    ($day_num:literal) => {
+        include_str!(concat!("../inputs/", $day_num, ".txt"))
+    };
 }
 
-pub fn day10(c: &mut Criterion) {
-    let s = read_to_string("./inputs/10.txt").unwrap();
-    let s = s.as_str();
+macro_rules! benches_day {
+    ($day_num:literal) => {
+        paste! {
+            use solution::[<day $day_num>];
 
-    c.bench_function("bench_day10_part1", |b| {
-        b.iter(|| day10::part1(black_box(s)))
-    });
-    c.bench_function("bench_day10_part2", |b| {
-        b.iter(|| day10::part2(black_box(s)))
-    });
+            pub fn [<bench_day $day_num>](c: &mut Criterion) {
+                let input = get_day_input!($day_num);
+                c.bench_function(&format!("bench_day{}_part1", $day_num), |b| b.iter(|| [<day $day_num>]::part1(black_box(input))));
+                c.bench_function(&format!("bench_day{}_part2", $day_num), |b| b.iter(|| [<day $day_num>]::part2(black_box(input))));
+            }
+        }
+    };
 }
 
-pub fn day11(c: &mut Criterion) {
-    let s = read_to_string("./inputs/11.txt").unwrap();
-    let s = s.as_str();
+macro_rules! benches {
+    ($($day_num:literal),*) => {
+        paste! {
+            $(
+                benches_day!($day_num);
+            )*
 
-    c.bench_function("bench_day11_part1", |b| {
-        b.iter(|| day11::part1(black_box(s)))
-    });
-    c.bench_function("bench_day11_part2", |b| {
-        b.iter(|| day11::part2(black_box(s)))
-    });
+            criterion_group!(benches, $([<bench_day $day_num>]),*);
+            criterion_main!(benches);
+        }
+    };
 }
 
-pub fn day12(c: &mut Criterion) {
-    let s = read_to_string("./inputs/12.txt").unwrap();
-    let s = s.as_str();
-
-    c.bench_function("bench_day12_part1", |b| {
-        b.iter(|| day12::part1(black_box(s)))
-    });
-    c.bench_function("bench_day12_part2", |b| {
-        b.iter(|| day12::part2(black_box(s)))
-    });
-}
-
-pub fn day13(c: &mut Criterion) {
-    let s = read_to_string("./inputs/13.txt").unwrap();
-    let s = s.as_str();
-
-    c.bench_function("bench_day13_part1", |b| {
-        b.iter(|| day13::part1(black_box(s)))
-    });
-    c.bench_function("bench_day13_part2", |b| {
-        b.iter(|| day13::part2(black_box(s)))
-    });
-}
-
-pub fn day14(c: &mut Criterion) {
-    let s = read_to_string("./inputs/14.txt").unwrap();
-    let s = s.as_str();
-
-    c.bench_function("bench_day14_part1", |b| {
-        b.iter(|| day14::part1(black_box(s)))
-    });
-    c.bench_function("bench_day14_part2", |b| {
-        b.iter(|| day14::part2(black_box(s)))
-    });
-}
-
-criterion_group!(benches, day9, day10, day11, day12, day13, day14);
-criterion_main!(benches);
+benches!(9, 10, 11, 12, 13, 14);
