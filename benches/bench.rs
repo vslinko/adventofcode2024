2775d65446abea85b302de0c208ee78ba384fe55
Viacheslav Slinko <me@vslinko.ru>
2024-12-15 22:40:30 +0300

    Revert "Revert "day13_simd""
    This reverts commit 49418e4f37fb258f3446fec7e8f5d51b90db0516.

--- benches/bench.rs
+++ benches/bench.rs
@@ -2,36 +2,44 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use paste::paste;
 
 macro_rules! get_day_input {
-    ($day_num:literal) => {
-        include_str!(concat!("../inputs/", $day_num, ".txt"))
+    ($day_file:expr) => {
+        include_str!(concat!("../inputs/", $day_file))
     };
 }
 
 macro_rules! benches_day {
-    ($day_num:literal) => {
+    ($day_name:expr, $day_file:expr) => {
         paste! {
-            use solution::[<day $day_num>];
+            use solution::[<day $day_name>];
 
-            pub fn [<bench_day $day_num>](c: &mut Criterion) {
-                let input = get_day_input!($day_num);
-                c.bench_function(&format!("bench_day{}_part1", $day_num), |b| b.iter(|| [<day $day_num>]::part1(black_box(input))));
-                c.bench_function(&format!("bench_day{}_part2", $day_num), |b| b.iter(|| [<day $day_num>]::part2(black_box(input))));
+            pub fn [<bench_day $day_name>](c: &mut Criterion) {
+                let input = get_day_input!($day_file);
+                c.bench_function(&format!("bench_day{}_part1", $day_name), |b| b.iter(|| [<day $day_name>]::part1(black_box(input))));
+                c.bench_function(&format!("bench_day{}_part2", $day_name), |b| b.iter(|| [<day $day_name>]::part2(black_box(input))));
             }
         }
     };
 }
 
 macro_rules! benches {
-    ($($day_num:literal),*) => {
+    ($($day_name:expr, $day_file:expr),*) => {
         paste! {
             $(
-                benches_day!($day_num);
+                benches_day!($day_name, $day_file);
             )*
 
-            criterion_group!(benches, $([<bench_day $day_num>]),*);
+            criterion_group!(benches, $([<bench_day $day_name>]),*);
             criterion_main!(benches);
         }
     };
 }
 
-benches!(9, 10, 11, 12, 13, 14);
+#[rustfmt::skip]
+benches!(
+    9, "9.txt",
+    10, "10.txt",
+    11, "11.txt",
+    12, "12.txt",
+    13, "13.txt", "13_simd", "13.txt",
+    14, "14.txt"
+);
