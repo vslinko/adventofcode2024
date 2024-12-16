ef2c781d2587a1dd0630f29f71b6723cb2806349
Viacheslav Slinko <me@vslinko.ru>
2024-12-16 13:46:08 +0300

    day16

--- src/day16.rs
+++ src/day16.rs
@@ -6,15 +6,14 @@ const HEIGHT: usize = 141;
 const LINE_LENGTH: usize = WIDTH + 1;
 const START_X: usize = 1;
 const START_Y: usize = HEIGHT - 2;
+const START_INDEX: usize = START_Y * LINE_LENGTH + START_X;
 const END_X: usize = WIDTH - 2;
 const END_Y: usize = 1;
+const END_INDEX: usize = END_Y * LINE_LENGTH + END_X;
 
 #[derive(Eq, PartialEq)]
 struct Node {
-    f_score: usize,
-    g_score: usize,
-    x: usize,
-    y: usize,
+    score: usize,
     index: usize,
     direction: Direction,
 }
@@ -29,12 +28,7 @@ enum Direction {
 
 impl Ord for Node {
     fn cmp(&self, other: &Self) -> Ordering {
-        other
-            .f_score
-            .cmp(&self.f_score)
-            .then_with(|| other.g_score.cmp(&self.g_score))
-            .then_with(|| self.x.cmp(&other.x))
-            .then_with(|| self.y.cmp(&other.y))
+        other.score.cmp(&self.score)
     }
 }
 
@@ -44,39 +38,30 @@ impl PartialOrd for Node {
     }
 }
 
-fn get_index(x: usize, y: usize) -> usize {
-    y * LINE_LENGTH + x
-}
-
-fn manhattan_distance(x1: usize, y1: usize, x2: usize, y2: usize) -> usize {
-    ((x1 as isize - x2 as isize).abs() + (y1 as isize - y2 as isize).abs()) as usize
-}
-
 fn get_neighbors_with_direction(
-    x: usize,
-    y: usize,
+    index: usize,
     current_dir: Direction,
-) -> [(usize, usize, Direction, usize); 3] {
+) -> [(usize, Direction, usize); 3] {
     match current_dir {
         Direction::East => [
-            (x + 1, y, Direction::East, 1),
-            (x, y - 1, Direction::North, 1001),
-            (x, y + 1, Direction::South, 1001),
+            (index + 1, Direction::East, 1),
+            (index - LINE_LENGTH, Direction::North, 1001),
+            (index + LINE_LENGTH, Direction::South, 1001),
         ],
         Direction::West => [
-            (x - 1, y, Direction::West, 1),
-            (x, y - 1, Direction::North, 1001),
-            (x, y + 1, Direction::South, 1001),
+            (index - 1, Direction::West, 1),
+            (index - LINE_LENGTH, Direction::North, 1001),
+            (index + LINE_LENGTH, Direction::South, 1001),
         ],
         Direction::North => [
-            (x, y - 1, Direction::North, 1),
-            (x - 1, y, Direction::West, 1001),
-            (x + 1, y, Direction::East, 1001),
+            (index - LINE_LENGTH, Direction::North, 1),
+            (index - 1, Direction::West, 1001),
+            (index + 1, Direction::East, 1001),
         ],
         Direction::South => [
-            (x, y + 1, Direction::South, 1),
-            (x - 1, y, Direction::West, 1001),
-            (x + 1, y, Direction::East, 1001),
+            (index + LINE_LENGTH, Direction::South, 1),
+            (index - 1, Direction::West, 1001),
+            (index + 1, Direction::East, 1001),
         ],
     }
 }
@@ -92,29 +77,24 @@ unsafe fn inner1(input: &str) -> usize {
     let mut closed_set = HashSet::new();
 
     let start_node = Node {
-        f_score: manhattan_distance(START_X, START_Y, END_X, END_Y),
-        g_score: 0,
-        x: START_X,
-        y: START_Y,
-        index: get_index(START_X, START_Y),
+        score: 0,
+        index: START_INDEX,
         direction: Direction::East,
     };
     open_set.push(start_node);
 
     while let Some(current) = open_set.pop() {
-        if current.x == END_X && current.y == END_Y {
-            return current.g_score;
+        if current.index == END_INDEX {
+            return current.score;
         }
 
         if !closed_set.insert(current.index) {
             continue;
         }
 
-        for (next_x, next_y, next_dir, next_score) in
-            get_neighbors_with_direction(current.x, current.y, current.direction)
+        for (next_index, next_dir, next_score) in
+            get_neighbors_with_direction(current.index, current.direction)
         {
-            let next_index = get_index(next_x, next_y);
-
             if input[next_index] == b'#' {
                 continue;
             }
@@ -123,14 +103,8 @@ unsafe fn inner1(input: &str) -> usize {
                 continue;
             }
 
-            let next_g_score = current.g_score + next_score;
-            let next_h_score = manhattan_distance(next_x, next_y, END_X, END_Y);
-
             let next_node = Node {
-                f_score: next_g_score + next_h_score,
-                g_score: next_g_score,
-                x: next_x,
-                y: next_y,
+                score: current.score + next_score,
                 index: next_index,
                 direction: next_dir,
             };
