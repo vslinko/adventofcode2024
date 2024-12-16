38eb964089f504f92e996c28dac3795bab43a9a2
Viacheslav Slinko <me@vslinko.ru>
2024-12-16 14:30:36 +0300

    day16

--- src/day16.rs
+++ src/day16.rs
@@ -12,19 +12,19 @@ const END_X: usize = WIDTH - 2;
 const END_Y: usize = 1;
 const END_INDEX: usize = END_Y * LINE_LENGTH + END_X;
 
-#[derive(Eq, PartialEq)]
-struct Node {
-    score: usize,
-    index: usize,
-    direction: Direction,
-}
-
 #[derive(Eq, PartialEq, Hash, Clone)]
 enum Direction {
     North,
     South,
     East,
     West,
+}
+
+#[derive(Eq, PartialEq)]
+struct Node {
+    score: usize,
+    index: usize,
+    direction: Direction,
 }
 
 impl Ord for Node {
@@ -39,7 +39,7 @@ impl PartialOrd for Node {
     }
 }
 
-fn get_neighbors_with_direction(
+fn get_neighbors_with_direction1(
     index: usize,
     current_dir: Direction,
 ) -> [(usize, Direction, usize); 3] {
@@ -67,13 +67,7 @@ fn get_neighbors_with_direction(
     }
 }
 
-pub fn part1(input: &str) -> usize {
-    unsafe { inner1(input) }
-}
-
-unsafe fn inner1(input: &str) -> usize {
-    let input = input.as_bytes();
-
+unsafe fn find_fastest_path_score(input: &[u8]) -> usize {
     let mut open_set = BinaryHeap::new();
     let mut closed_set = HashSet::new();
 
@@ -94,7 +88,7 @@ unsafe fn inner1(input: &str) -> usize {
         }
 
         for (next_index, next_dir, next_score) in
-            get_neighbors_with_direction(current.index, current.direction)
+            get_neighbors_with_direction1(current.index, current.direction)
         {
             if *input.get_unchecked(next_index) == b'#' {
                 continue;
@@ -117,189 +111,84 @@ unsafe fn inner1(input: &str) -> usize {
     usize::MAX
 }
 
-#[derive(Clone)]
-struct Point {
-    x: i32,
-    y: i32,
-    direction: Direction,
+pub fn part1(input: &str) -> usize {
+    unsafe { inner1(input) }
 }
 
-impl Point {
-    fn new(x: i32, y: i32, direction: Direction) -> Self {
-        Self { x, y, direction }
+unsafe fn inner1(input: &str) -> usize {
+    let input = input.as_bytes();
+
+    find_fastest_path_score(input)
+}
+
+fn get_neighbors_with_direction2(point: &(usize, Direction)) -> [(usize, Direction, usize); 3] {
+    match point.1 {
+        Direction::East => [
+            (point.0 + 1, Direction::East, 1),
+            (point.0, Direction::North, 1000),
+            (point.0, Direction::South, 1000),
+        ],
+        Direction::West => [
+            (point.0 - 1, Direction::West, 1),
+            (point.0, Direction::North, 1000),
+            (point.0, Direction::South, 1000),
+        ],
+        Direction::North => [
+            (point.0 - LINE_LENGTH, Direction::North, 1),
+            (point.0, Direction::West, 1000),
+            (point.0, Direction::East, 1000),
+        ],
+        Direction::South => [
+            (point.0 + LINE_LENGTH, Direction::South, 1),
+            (point.0, Direction::West, 1000),
+            (point.0, Direction::East, 1000),
+        ],
     }
-
-    fn hash(&self) -> String {
-        let direction = match self.direction {
-            Direction::North => 'N',
-            Direction::South => 'S',
-            Direction::East => 'E',
-            Direction::West => 'W',
-        };
-
-        format!("{},{},{}", self.x, self.y, direction)
-    }
 }
 
-struct DirectionMove {
-    direction: Direction,
-    dx: i32,
-    dy: i32,
-    score: i32,
-}
-
-fn get_directions() -> HashMap<Direction, Vec<DirectionMove>> {
-    let mut directions = HashMap::new();
-
-    directions.insert(
-        Direction::East,
-        vec![
-            DirectionMove {
-                direction: Direction::East,
-                dx: 1,
-                dy: 0,
-                score: 1,
-            },
-            DirectionMove {
-                direction: Direction::North,
-                dx: 0,
-                dy: 0,
-                score: 1000,
-            },
-            DirectionMove {
-                direction: Direction::South,
-                dx: 0,
-                dy: 0,
-                score: 1000,
-            },
-        ],
-    );
-
-    directions.insert(
-        Direction::North,
-        vec![
-            DirectionMove {
-                direction: Direction::North,
-                dx: 0,
-                dy: -1,
-                score: 1,
-            },
-            DirectionMove {
-                direction: Direction::West,
-                dx: 0,
-                dy: 0,
-                score: 1000,
-            },
-            DirectionMove {
-                direction: Direction::East,
-                dx: 0,
-                dy: 0,
-                score: 1000,
-            },
-        ],
-    );
-
-    directions.insert(
-        Direction::West,
-        vec![
-            DirectionMove {
-                direction: Direction::West,
-                dx: -1,
-                dy: 0,
-                score: 1,
-            },
-            DirectionMove {
-                direction: Direction::North,
-                dx: 0,
-                dy: 0,
-                score: 1000,
-            },
-            DirectionMove {
-                direction: Direction::South,
-                dx: 0,
-                dy: 0,
-                score: 1000,
-            },
-        ],
-    );
-
-    directions.insert(
-        Direction::South,
-        vec![
-            DirectionMove {
-                direction: Direction::South,
-                dx: 0,
-                dy: 1,
-                score: 1,
-            },
-            DirectionMove {
-                direction: Direction::West,
-                dx: 0,
-                dy: 0,
-                score: 1000,
-            },
-            DirectionMove {
-                direction: Direction::East,
-                dx: 0,
-                dy: 0,
-                score: 1000,
-            },
-        ],
-    );
-
-    directions
-}
-
-fn find_all_shortest_paths(
-    maze: &Vec<Vec<char>>,
-    start_x: i32,
-    start_y: i32,
-    start_direction: Direction,
-    end_x: i32,
-    end_y: i32,
-) -> Vec<(Vec<Point>, i32)> {
-    let start = Point::new(start_x, start_y, start_direction);
-    let directions = get_directions();
-    let mut all_paths = Vec::new();
+unsafe fn find_unique_cells_count_of_all_fastest_pathes(input: &[u8], max_score: usize) -> usize {
+    let start = (START_INDEX, Direction::East);
+    let mut unique_cells = HashSet::new();
     let mut queue = VecDeque::new();
     let mut scores = HashMap::new();
 
     queue.push_back((vec![start.clone()], 0));
-    scores.insert(start.hash(), 0);
+    scores.insert(start, 0_usize);
 
     while let Some((current_path, score)) = queue.pop_front() {
-        let current = current_path.last().unwrap();
+        let current = current_path.last().unwrap_unchecked();
 
-        if current.x == end_x && current.y == end_y {
-            all_paths.push((current_path, score));
+        if current.0 == END_INDEX {
+            for path in current_path {
+                unique_cells.insert(path.0);
+            }
             continue;
         }
 
-        for d in directions.get(&current.direction).unwrap() {
-            let next_x = current.x + d.dx;
-            let next_y = current.y + d.dy;
+        for (next_index, next_direction, next_score) in get_neighbors_with_direction2(&current) {
+            if *input.get_unchecked(next_index) != b'#' {
+                let new_score = score + next_score;
 
-            if maze[next_y as usize][next_x as usize] != '#' {
-                let next_point = Point::new(next_x, next_y, d.direction.clone());
-                let new_distance = score + d.score;
+                if new_score > max_score {
+                    continue;
+                }
 
-                let current_score = scores.get(&next_point.hash()).unwrap_or(&i32::MAX);
-                if new_distance <= *current_score {
-                    scores.insert(next_point.hash(), new_distance);
+                let next_point = (next_index, next_direction);
+
+                let current_score = scores.get(&next_point).unwrap_or(&usize::MAX);
+
+                if new_score <= *current_score {
+                    scores.insert(next_point.clone(), new_score);
+
                     let mut new_path = current_path.clone();
                     new_path.push(next_point);
-                    queue.push_back((new_path, new_distance));
+                    queue.push_back((new_path, new_score));
                 }
             }
         }
     }
 
-    let min_score = all_paths.iter().map(|(_, score)| *score).min().unwrap_or(0);
-
-    all_paths
-        .into_iter()
-        .filter(|(_, score)| *score == min_score)
-        .collect()
+    unique_cells.len()
 }
 
 pub fn part2(input: &str) -> usize {
@@ -307,47 +196,11 @@ pub fn part2(input: &str) -> usize {
 }
 
 unsafe fn inner2(input: &str) -> usize {
-    let lines: Vec<Vec<char>> = input
-        .trim()
-        .lines()
-        .map(|line| line.trim().chars().collect())
-        .collect();
+    let input = input.as_bytes();
 
-    assert!(!lines.is_empty());
+    let max_score = find_fastest_path_score(&input);
 
-    let m = lines[0].len();
-    let n = lines.len();
-
-    let start_x = 1;
-    let start_y = (n - 2) as i32;
-    let end_x = (m - 2) as i32;
-    let end_y = 1;
-
-    assert_eq!(lines[start_y as usize][start_x], 'S');
-    assert_eq!(lines[end_y as usize][end_x as usize], 'E');
-
-    let mut lines = lines;
-    lines[start_y as usize][start_x] = '.';
-    lines[end_y as usize][end_x as usize] = '.';
-
-    let shortest_paths = find_all_shortest_paths(
-        &lines,
-        start_x as i32,
-        start_y,
-        Direction::East,
-        end_x,
-        end_y,
-    );
-
-    let mut unique_cells = HashSet::new();
-
-    for (path, _) in shortest_paths {
-        for point in path {
-            unique_cells.insert(format!("{},{}", point.x, point.y));
-        }
-    }
-
-    unique_cells.len()
+    find_unique_cells_count_of_all_fastest_pathes(&input, max_score)
 }
 
 #[cfg(test)]
