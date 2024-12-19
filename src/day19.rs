160ecf67d90878f40635e8f8738015528b864397
Viacheslav Slinko <me@vslinko.ru>
2024-12-19 13:52:31 +0300

    day19

--- src/day19.rs
+++ src/day19.rs
@@ -1,11 +1,7 @@
 use rayon::prelude::*;
 use rustc_hash::{FxBuildHasher, FxHashMap, FxHashSet};
 
-pub fn part1(input: &str) -> usize {
-    unsafe { inner1(input) }
-}
-
-unsafe fn inner1(input: &str) -> usize {
+unsafe fn parse(input: &str) -> (FxHashMap<usize, Vec<&str>>, Vec<&str>) {
     let mut lines = input.lines();
 
     let patterns_by_length: FxHashMap<usize, Vec<&str>> =
@@ -21,6 +17,16 @@ unsafe fn inner1(input: &str) -> usize {
     lines.next();
 
     let designs: Vec<&str> = lines.collect();
+
+    (patterns_by_length, designs)
+}
+
+pub fn part1(input: &str) -> usize {
+    unsafe { inner1(input) }
+}
+
+unsafe fn inner1(input: &str) -> usize {
+    let (patterns_by_length, designs) = parse(input);
 
     designs
         .par_iter()
@@ -28,48 +34,48 @@ unsafe fn inner1(input: &str) -> usize {
         .count()
 }
 
-fn can_make_design_recursive(
-    pos: usize,
-    design: &[u8],
-    patterns_by_length: &FxHashMap<usize, Vec<&str>>,
-    memo: &mut FxHashSet<usize>,
-) -> bool {
-    if pos == design.len() {
-        return true;
-    }
-
-    if memo.contains(&pos) {
-        return false;
-    }
-
-    for (&length, patterns) in patterns_by_length.iter() {
-        let next_pos = pos + length;
-
-        if next_pos > design.len() {
-            continue;
+fn can_make_design(design: &str, patterns_by_length: &FxHashMap<usize, Vec<&str>>) -> bool {
+    fn can_make_design_recursive(
+        pos: usize,
+        design: &[u8],
+        patterns_by_length: &FxHashMap<usize, Vec<&str>>,
+        memo: &mut FxHashSet<usize>,
+    ) -> bool {
+        if pos == design.len() {
+            return true;
         }
 
-        'pattern_loop: for pattern in patterns {
-            for (j, &pattern_byte) in pattern.as_bytes().iter().enumerate() {
-                if design[pos + j] != pattern_byte {
-                    continue 'pattern_loop;
+        if memo.contains(&pos) {
+            return false;
+        }
+
+        for (&length, patterns) in patterns_by_length.iter() {
+            let next_pos = pos + length;
+
+            if next_pos > design.len() {
+                continue;
+            }
+
+            'pattern_loop: for pattern in patterns {
+                for (j, &b) in pattern.as_bytes().iter().enumerate() {
+                    if design[pos + j] != b {
+                        continue 'pattern_loop;
+                    }
+                }
+
+                if can_make_design_recursive(next_pos, design, patterns_by_length, memo) {
+                    return true;
                 }
             }
-
-            if can_make_design_recursive(next_pos, design, patterns_by_length, memo) {
-                return true;
-            }
         }
+
+        memo.insert(pos);
+        false
     }
 
-    memo.insert(pos);
-    false
-}
-
-fn can_make_design(design: &str, patterns_by_length: &FxHashMap<usize, Vec<&str>>) -> bool {
-    let design_bytes = design.as_bytes();
     let mut memo = FxHashSet::with_hasher(FxBuildHasher::default());
-    can_make_design_recursive(0, design_bytes, patterns_by_length, &mut memo)
+
+    can_make_design_recursive(0, design.as_bytes(), patterns_by_length, &mut memo)
 }
 
 pub fn part2(input: &str) -> u64 {
@@ -77,21 +83,7 @@ pub fn part2(input: &str) -> u64 {
 }
 
 unsafe fn inner2(input: &str) -> u64 {
-    let mut lines = input.lines();
-
-    let patterns_by_length: FxHashMap<usize, Vec<&str>> =
-        lines.next().unwrap_unchecked().split(',').fold(
-            FxHashMap::with_capacity_and_hasher(8, FxBuildHasher::default()),
-            |mut acc, pattern| {
-                let pattern = pattern.trim_start();
-                acc.entry(pattern.len()).or_default().push(pattern);
-                acc
-            },
-        );
-
-    lines.next();
-
-    let designs: Vec<&str> = lines.collect();
+    let (patterns_by_length, designs) = parse(input);
 
     designs
         .iter()
@@ -103,21 +95,19 @@ fn count_possible_combinations(
     design: &str,
     patterns_by_length: &FxHashMap<usize, Vec<&str>>,
 ) -> u64 {
-    let design_bytes = design.as_bytes();
-    let mut dp = vec![0; design_bytes.len() + 1];
-    dp[design_bytes.len()] = 1;
+    let design = design.as_bytes();
+    let mut dp = vec![0; design.len() + 1];
+    dp[design.len()] = 1;
 
-    for i in (0..design_bytes.len()).rev() {
+    for i in (0..design.len()).rev() {
         for (&length, patterns) in patterns_by_length.iter() {
-            if design_bytes.len() - i < length {
+            if design.len() - i < length {
                 continue;
             }
 
             'pattern_loop: for pattern in patterns {
-                let pattern_bytes = pattern.as_bytes();
-
-                for (j, &pattern_byte) in pattern_bytes.iter().enumerate() {
-                    if design_bytes[i + j] != pattern_byte {
+                for (j, &b) in pattern.as_bytes().iter().enumerate() {
+                    if design[i + j] != b {
                         continue 'pattern_loop;
                     }
                 }
