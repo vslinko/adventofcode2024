1ede0e5e69ab329038754224bd169f142c210977
Viacheslav Slinko <me@vslinko.ru>
2024-12-20 19:07:08 +0300

    day20

--- src/day20.rs
+++ src/day20.rs
@@ -30,7 +30,7 @@ unsafe fn calc_distances(grid: &[u8], pos: usize, dist: usize, distances: &mut [
 }
 
 fn distance(x1: usize, y1: usize, x2: usize, y2: usize) -> usize {
-    (x1.max(x2) - x1.min(x2)) + (y1.max(y2) - y1.min(y2))
+    (x1 as isize - x2 as isize).abs() as usize + (y1 as isize - y2 as isize).abs() as usize
 }
 
 unsafe fn solve(input: &str, max_cheating_time: usize) -> usize {
