a16f2c02554df643919d3b5f3431b08bebc3e003
Viacheslav Slinko <me@vslinko.ru>
2024-12-22 00:15:53 +0300

    day21

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,25 @@
 #![feature(portable_simd)]
 
-pub mod day1;
-pub mod day2;
-pub mod day3;
-pub mod day4;
-pub mod day5;
-pub mod day6;
-pub mod day7;
-pub mod day8;
-pub mod day9;
+// pub mod day1;
+// pub mod day2;
+// pub mod day3;
+// pub mod day4;
+// pub mod day5;
+// pub mod day6;
+// pub mod day7;
+// pub mod day8;
+// pub mod day9;
 
-pub mod day10;
-pub mod day11;
-pub mod day12;
-pub mod day13;
-pub mod day13_simd;
-pub mod day14;
-pub mod day15;
-pub mod day16;
-pub mod day17;
-pub mod day18;
-pub mod day19;
-pub mod day20;
+// pub mod day10;
+// pub mod day11;
+// pub mod day12;
+// pub mod day13;
+// pub mod day13_simd;
+// pub mod day14;
+// pub mod day15;
+// pub mod day16;
+// pub mod day17;
+// pub mod day18;
+// pub mod day19;
+// pub mod day20;
 pub mod day21;
