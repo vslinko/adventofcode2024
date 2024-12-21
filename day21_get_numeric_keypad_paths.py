682d986a7110887531edb50ada2a980f6ac72c2e
Viacheslav Slinko <me@vslinko.ru>
2024-12-21 15:43:28 +0300

    day21

---
+++ day21_get_numeric_keypad_paths.py
@@ -0,0 +1,102 @@
+# 0 = 48
+# 1 = 49
+# 2 = 50
+# 3 = 51
+# 4 = 52
+# 5 = 53
+# 6 = 54
+# 7 = 55
+# 8 = 56
+# 9 = 57
+# A = 65
+
+# +---+---+---+
+# | 7 | 8 | 9 |
+# +---+---+---+
+# | 4 | 5 | 6 |
+# +---+---+---+
+# | 1 | 2 | 3 |
+# +---+---+---+
+#     | 0 | A |
+#     +---+---+
+COORDS = {
+    48: (1, 3),
+    49: (0, 2),
+    50: (1, 2),
+    51: (2, 2),
+    52: (0, 1),
+    53: (1, 1),
+    54: (2, 1),
+    55: (0, 0),
+    56: (1, 0),
+    57: (2, 0),
+    65: (2, 3),
+}
+
+all_moves = {}
+
+all_keys = []
+max_key = 0
+
+for a in COORDS.keys():
+    for b in COORDS.keys():
+        k = a-48 + (b-48)*18
+        all_keys.append(k)
+        max_key = max(max_key, k)
+
+        a_coords = COORDS[a]
+        b_coords = COORDS[b]
+
+        dx = b_coords[0] - a_coords[0]
+        dy = b_coords[1] - a_coords[1]
+
+        moves = []
+        if dx > 0:
+            moves.append(">" * dx)
+        elif dx < 0:
+            moves.append("<" * -dx)
+
+        if dy > 0:
+            moves.append("v" * dy)
+        elif dy < 0:
+            moves.append("^" * -dy)
+
+        moves = "".join(moves)
+        moves = list(set([moves, moves[::-1]]))
+
+        if a_coords[0] == 0 and b_coords[1] == 3:
+            moves = [m for m in moves if m[0] != "v"]
+
+        if a_coords[1] == 3 and b_coords[0] == 0:
+            moves = [m for m in moves if m[0] != "<"]
+
+        moves = list(sorted([m + "A" for m in moves]))
+
+        agg_key = " ".join(["".join(m) for m in moves])
+        if agg_key not in all_moves:
+            all_moves[agg_key] = {
+                "moves": moves,
+                "keys": [],
+                "comments": [],
+            }
+
+        all_moves[agg_key]["keys"].append(str(k))
+        all_moves[agg_key]["comments"].append(chr(a) + chr(b))
+
+print('fn get_numeric_keypad_paths(from: u8, to: u8) -> Vec<Vec<u8>> {')
+print('    match (from as u16) - 48 + ((to as u16) - 48) * 18 {')
+for group in all_moves.values():
+    keys = " | ".join(group["keys"])
+    moves = ", ".join([
+        "vec![{}]".format(", ".join([
+            "b'{}'".format(c)
+            for c in list(m)
+        ]))
+        for m in group["moves"]
+    ])
+    comments = " ".join(group["comments"])
+    print("        {} => vec![{}], // {}".format(keys, moves, comments))
+
+print("        _ => vec![],")
+print("    }")
+print("}")
--- src/day21.rs
+++ src/day21.rs
@@ -1,66 +1,96 @@
 use rustc_hash::{FxBuildHasher, FxHashMap};
 
 fn get_numeric_keypad_paths(from: u8, to: u8) -> Vec<Vec<u8>> {
-    match (from, to) {
-        (b'A', b'7') => vec![vec![b'^', b'^', b'^', b'<', b'<', b'A']],
-        (b'7', b'8') => vec![vec![b'>', b'A']],
-        (b'8', b'9') => vec![vec![b'>', b'A']],
-        (b'9', b'A') => vec![vec![b'v', b'v', b'v', b'A']],
-        (b'A', b'0') => vec![vec![b'<', b'A']],
-        (b'0', b'2') => vec![vec![b'^', b'A']],
-        (b'2', b'9') => vec![vec![b'^', b'^', b'>', b'A'], vec![b'>', b'^', b'^', b'A']],
-        (b'A', b'9') => vec![vec![b'^', b'^', b'^', b'A']],
-        (b'9', b'8') => vec![vec![b'<', b'A']],
-        (b'8', b'0') => vec![vec![b'v', b'v', b'v', b'A']],
-        (b'0', b'A') => vec![vec![b'>', b'A']],
-        (b'A', b'1') => vec![vec![b'^', b'<', b'<', b'A']],
-        (b'1', b'7') => vec![vec![b'^', b'^', b'A']],
-        (b'7', b'9') => vec![vec![b'>', b'>', b'A']],
-        (b'A', b'4') => vec![vec![b'^', b'^', b'<', b'<', b'A']],
-        (b'4', b'5') => vec![vec![b'>', b'A']],
-        (b'5', b'6') => vec![vec![b'>', b'A']],
-        (b'6', b'A') => vec![vec![b'v', b'v', b'A']],
-        (b'A', b'3') => vec![vec![b'^', b'A']],
-        (b'3', b'7') => vec![
-            vec![b'^', b'^', b'<', b'<', b'A'],
+    match (from as u16) - 48 + ((to as u16) - 48) * 18 {
+        0 | 19 | 38 | 57 | 76 | 95 | 114 | 133 | 152 | 171 | 323 => vec![vec![b'A']], // 00 11 22 33 44 55 66 77 88 99 AA
+        18 => vec![vec![b'^', b'<', b'A']],                                           // 01
+        36 | 73 | 92 | 111 | 130 | 149 | 168 | 71 => vec![vec![b'^', b'A']], // 02 14 25 36 47 58 69 A3
+        54 | 91 | 110 | 148 | 167 => vec![vec![b'>', b'^', b'A'], vec![b'^', b'>', b'A']], // 03 15 26 48 59
+        72 => vec![vec![b'^', b'^', b'<', b'A']],                                          // 04
+        90 | 127 | 146 | 165 | 125 => vec![vec![b'^', b'^', b'A']], // 05 17 28 39 A6
+        108 | 145 | 164 => vec![vec![b'>', b'^', b'^', b'A'], vec![b'^', b'^', b'>', b'A']], // 06 18 29
+        126 => vec![vec![b'^', b'^', b'^', b'<', b'A']],                                     // 07
+        144 | 179 => vec![vec![b'^', b'^', b'^', b'A']], // 08 A9
+        162 => vec![
+            vec![b'>', b'^', b'^', b'^', b'A'],
+            vec![b'^', b'^', b'^', b'>', b'A'],
+        ], // 09
+        306 | 37 | 56 | 94 | 113 | 151 | 170 => vec![vec![b'>', b'A']], // 0A 12 23 45 56 78 89
+        1 => vec![vec![b'>', b'v', b'A']],               // 10
+        55 | 112 | 169 => vec![vec![b'>', b'>', b'A']],  // 13 46 79
+        109 | 166 => vec![vec![b'>', b'>', b'^', b'A'], vec![b'^', b'>', b'>', b'A']], // 16 49
+        163 => vec![
+            vec![b'>', b'>', b'^', b'^', b'A'],
+            vec![b'^', b'^', b'>', b'>', b'A'],
+        ], // 19
+        307 => vec![vec![b'>', b'>', b'v', b'A']],       // 1A
+        2 | 309 | 22 | 41 | 60 | 79 | 98 | 117 => vec![vec![b'v', b'A']], // 20 3A 41 52 63 74 85 96
+        20 | 39 | 77 | 96 | 134 | 153 | 17 => vec![vec![b'<', b'A']], // 21 32 54 65 87 98 A0
+        74 | 93 | 131 | 150 | 53 => vec![vec![b'<', b'^', b'A'], vec![b'^', b'<', b'A']], // 24 35 57 68 A2
+        128 | 147 | 107 => vec![vec![b'<', b'^', b'^', b'A'], vec![b'^', b'^', b'<', b'A']], // 27 38 A5
+        308 | 40 | 59 | 97 | 116 => vec![vec![b'>', b'v', b'A'], vec![b'v', b'>', b'A']], // 2A 42 53 75 86
+        3 | 23 | 42 | 80 | 99 => vec![vec![b'<', b'v', b'A'], vec![b'v', b'<', b'A']], // 30 51 62 84 95
+        21 | 78 | 135 => vec![vec![b'<', b'<', b'A']],                                 // 31 64 97
+        75 | 132 => vec![vec![b'<', b'<', b'^', b'A'], vec![b'^', b'<', b'<', b'A']],  // 34 67
+        129 => vec![
             vec![b'<', b'<', b'^', b'^', b'A'],
-        ],
-        (b'9', b'6') => vec![vec![b'v', b'A']],
-        (b'6', b'8') => vec![vec![b'^', b'<', b'A'], vec![b'<', b'^', b'A']],
-        (b'8', b'A') => vec![
-            vec![b'v', b'v', b'v', b'>', b'A'],
+            vec![b'^', b'^', b'<', b'<', b'A'],
+        ], // 37
+        4 => vec![vec![b'>', b'v', b'v', b'A']],                                       // 40
+        58 | 115 => vec![vec![b'>', b'>', b'v', b'A'], vec![b'v', b'>', b'>', b'A']],  // 43 76
+        310 => vec![vec![b'>', b'>', b'v', b'v', b'A']],                               // 4A
+        5 | 312 | 25 | 44 | 63 => vec![vec![b'v', b'v', b'A']], // 50 6A 71 82 93
+        311 | 43 | 62 => vec![vec![b'>', b'v', b'v', b'A'], vec![b'v', b'v', b'>', b'A']], // 5A 72 83
+        6 | 26 | 45 => vec![vec![b'<', b'v', b'v', b'A'], vec![b'v', b'v', b'<', b'A']], // 60 81 92
+        24 | 81 => vec![vec![b'<', b'<', b'v', b'A'], vec![b'v', b'<', b'<', b'A']],     // 61 94
+        7 => vec![vec![b'>', b'v', b'v', b'v', b'A']],                                   // 70
+        61 => vec![
+            vec![b'>', b'>', b'v', b'v', b'A'],
+            vec![b'v', b'v', b'>', b'>', b'A'],
+        ], // 73
+        313 => vec![vec![b'>', b'>', b'v', b'v', b'v', b'A']],                           // 7A
+        8 | 315 => vec![vec![b'v', b'v', b'v', b'A']],                                   // 80 9A
+        314 => vec![
             vec![b'>', b'v', b'v', b'v', b'A'],
-        ],
-        (b'A', b'2') => vec![vec![b'^', b'<', b'A'], vec![b'<', b'^', b'A']],
-        (b'2', b'8') => vec![vec![b'^', b'^', b'A']],
-        (b'8', b'6') => vec![vec![b'>', b'v', b'A'], vec![b'v', b'>', b'A']],
-        (b'3', b'4') => vec![vec![b'^', b'<', b'<', b'A'], vec![b'<', b'<', b'^', b'A']],
-        (b'4', b'9') => vec![vec![b'^', b'>', b'>', b'A'], vec![b'>', b'>', b'^', b'A']],
-        (b'7', b'0') => vec![vec![b'>', b'v', b'v', b'v', b'A']],
-        _ => panic!("Unknown move from {} to {}", from as char, to as char),
+            vec![b'v', b'v', b'v', b'>', b'A'],
+        ], // 8A
+        9 => vec![
+            vec![b'<', b'v', b'v', b'v', b'A'],
+            vec![b'v', b'v', b'v', b'<', b'A'],
+        ], // 90
+        27 => vec![
+            vec![b'<', b'<', b'v', b'v', b'A'],
+            vec![b'v', b'v', b'<', b'<', b'A'],
+        ], // 91
+        35 => vec![vec![b'^', b'<', b'<', b'A']],                                        // A1
+        89 => vec![vec![b'^', b'^', b'<', b'<', b'A']],                                  // A4
+        143 => vec![vec![b'^', b'^', b'^', b'<', b'<', b'A']],                           // A7
+        161 => vec![
+            vec![b'<', b'^', b'^', b'^', b'A'],
+            vec![b'^', b'^', b'^', b'<', b'A'],
+        ], // A8
+        _ => vec![],
     }
 }
 
 fn get_direction_keypad_paths(from: u8, to: u8) -> Vec<Vec<u8>> {
-    match (from, to) {
-        (f, t) if f == t => vec![vec![b'A']],
-        (b'A', b'<') => vec![vec![b'v', b'<', b'<', b'A']],
-        (b'<', b'A') => vec![vec![b'>', b'>', b'^', b'A']],
-        (b'A', b'v') => vec![vec![b'v', b'<', b'A'], vec![b'<', b'v', b'A']],
-        (b'v', b'<') => vec![vec![b'<', b'A']],
-        (b'A', b'>') => vec![vec![b'v', b'A']],
-        (b'>', b'^') => vec![vec![b'<', b'^', b'A'], vec![b'^', b'<', b'A']],
-        (b'^', b'A') => vec![vec![b'>', b'A']],
-        (b'A', b'^') => vec![vec![b'<', b'A']],
-        (b'>', b'A') => vec![vec![b'^', b'A']],
-        (b'^', b'>') => vec![vec![b'v', b'>', b'A'], vec![b'>', b'v', b'A']],
-        (b'v', b'>') => vec![vec![b'>', b'A']],
-        (b'v', b'A') => vec![vec![b'>', b'^', b'A'], vec![b'^', b'>', b'A']],
-        (b'<', b'^') => vec![vec![b'>', b'^', b'A']],
-        (b'^', b'<') => vec![vec![b'v', b'<', b'A']],
-        (b'>', b'v') => vec![vec![b'<', b'A']],
-        (b'<', b'v') => vec![vec![b'>', b'A']],
-        _ => panic!("Unknown move from {} to {}", from as char, to as char),
+    match from - 60 + (to - 60) * 3 {
+        0 | 8 | 20 | 136 | 232 => vec![vec![b'A']], // << >> AA ^^ vv
+        6 => vec![vec![b'>', b'>', b'A']],          // <>
+        15 => vec![vec![b'>', b'>', b'^', b'A']],   // <A
+        102 => vec![vec![b'>', b'^', b'A']],        // <^
+        174 | 49 | 64 => vec![vec![b'>', b'A']],    // <v ^A v>
+        2 => vec![vec![b'<', b'<', b'A']],          // ><
+        17 | 160 => vec![vec![b'^', b'A']],         // >A v^
+        104 => vec![vec![b'<', b'^', b'A'], vec![b'^', b'<', b'A']], // >^
+        176 | 107 | 58 => vec![vec![b'<', b'A']],   // >v A^ v<
+        5 => vec![vec![b'v', b'<', b'<', b'A']],    // A<
+        11 | 208 => vec![vec![b'v', b'A']],         // A> ^v
+        179 => vec![vec![b'<', b'v', b'A'], vec![b'v', b'<', b'A']], // Av
+        34 => vec![vec![b'v', b'<', b'A']],         // ^<
+        40 => vec![vec![b'>', b'v', b'A'], vec![b'v', b'>', b'A']], // ^>
+        73 => vec![vec![b'>', b'^', b'A'], vec![b'^', b'>', b'A']], // vA
+        _ => vec![],
     }
 }
 
